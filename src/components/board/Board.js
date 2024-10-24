509bf630b7c7d623fb96a0e20163bf15cb45b858
mohammedaydi <mohammed7.aydi@gmail.com>
2024-10-24 09:16:38 +0300

    concatenated the tasks' arrays

--- src/components/board/Board.js
+++ src/components/board/Board.js
@@ -8,23 +8,15 @@ const Board = (props) => {
   const uncompletedTasks = props.cards.filter((card) => {
     return card.done === false;
   });
+
+  const sortedArray = uncompletedTasks.concat(completedTasks);
   return (
     <div className="board">
       <div className="board-header">
         <h1>Todo List</h1>
       </div>
       <div className="board-cards">
-        {uncompletedTasks.map((card) => (
-          <Card
-            key={card.id}
-            id={card.id}
-            title={card.title}
-            isDone={card.done}
-            doneHandler={props.handlers.doneHandler}
-            deleteHandler={props.handlers.deleteHandler}
-          />
-        ))}
-        {completedTasks.map((card) => (
+        {sortedArray.map((card) => (
           <Card
             key={card.id}
             id={card.id}
